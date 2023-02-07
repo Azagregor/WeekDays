ea9ab2045dea62371ae0a33c668b2edcb7a6376f
Azagregor <akimbekov.azamat@mail.ru>
2023-02-07 20:29:03 +0600

    First Commit
    First Commit

---
+++ src/Weekday.java
@@ -0,0 +1,19 @@
+public class Weekday {
+    public static void main(String[] args) {
+        if ("1".equals(args[0])) {
+            System.out.println("понедельник");
+        } else if ("2".equals(args[0])) {
+            System.out.println("Вторник");
+        } else if ("3".equals(args[0])) {
+            System.out.println("Среда");
+        } else if ("4".equals(args[0])) {
+            System.out.println("Четверг");
+        } else if ("5".equals(args[0])) {
+            System.out.println("Пятница");
+        } else if ("6".equals(args[0]) || "7".equals(args[0])) {
+            System.out.println("Выходные");
+        } else {
+            System.out.println(" ");
+        }
+    }
+}
